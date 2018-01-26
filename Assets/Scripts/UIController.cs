22cb0261d3f1d9f0c88e6c0d8a12e5704d1d1a37
siimer <siimraudsep@gmail.com>
2018-01-26 21:25:09 +0200

    Added UI to test time and score

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+	[SerializeField] private Text timeText;
+	[SerializeField] private Text scoreText;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		scoreText.text = GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>().CurrentScore.ToString();
+		timeText.text = Mathf.Floor(Time.timeSinceLevelLoad).ToString(	);
+	}
+}
