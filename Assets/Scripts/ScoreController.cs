c400ec089fad21c14ac888a5b58a6ca159ffe12c
siimer <siimraudsep@gmail.com>
2018-01-26 19:55:19 +0200

    Moved scripts under 1 folder

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController
+{
+	private int currentScore = 0;
+	
+	public int CurrentScore
+	{
+		get { return currentScore; }
+		set { currentScore = value; }
+	}
+
+
+	public void addScore(int score)
+	{
+		CurrentScore += score;
+	}
+}
