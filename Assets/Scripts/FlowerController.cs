895562747a969b654b6d05f58b5d517c41161abd
siimer <siimraudsep@gmail.com>
2018-01-26 21:18:18 +0200

    Updated bee prefab with player tag, added flowercontroller

--- Assets/Scripts/FlowerController.cs
+++ Assets/Scripts/FlowerController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlowerController : MonoBehaviour {
+public class FlowerController : MonoBehaviour
+{
 
+	private const int SCORE_PICK = 10;
 	// Use this for initialization
 	void Start () {
 		
@@ -16,8 +18,11 @@ public class FlowerController : MonoBehaviour {
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
-		
-		print(other.gameObject.tag);
-		Destroy(gameObject);
+		if (other.gameObject.CompareTag("Player"))
+		{
+			GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>().CurrentScore += SCORE_PICK;
+			Destroy(gameObject);
+		}
+
 	}
 }
