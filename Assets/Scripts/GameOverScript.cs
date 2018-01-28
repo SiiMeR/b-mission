4a1a11cd52dfd3c14b3450f8406050940993120e
siimer <siimraudsep@gmail.com>
2018-01-28 12:25:05 +0200

    name getting works from prompt

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -7,12 +7,20 @@ public class GameOverScript : MonoBehaviour
 {
 
 	[SerializeField] private GameObject[] highscores;
-	
+
+	[SerializeField] private InputField name;
+
+	private string playerName = "";
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	public void getName()
+	{
+		playerName = name.text;
+		print("sain nime : " + playerName);
+	}
 	// Update is called once per frame
 	void Update () {
 		
