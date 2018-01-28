acb03f1aa5dc5c24c8f4c06eac48e52613dd2f26
siimer <siimraudsep@gmail.com>
2018-01-28 11:36:13 +0200

    Shipped highscore UI and initial prompt component

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+
+	[SerializeField] private GameObject[] highscores;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
